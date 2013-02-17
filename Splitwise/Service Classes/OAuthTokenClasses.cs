f563ae7453c355a42971f00f80b8219b9b357b5a
Swathi Sundar <swathisundar@outlook.com>
2013-02-17 01:39:49 -0800

    first commit

---
+++ Splitwise/Service Classes/OAuthTokenClasses.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitwise_New.Service_Classes
+{
+    #region Request Token Class
+    class RequestToken
+    {
+        string requestToken;
+        string oauth_token;
+
+        public string OauthToken
+        {
+            get { return oauth_token; }
+            set { oauth_token = value; }
+        }
+        string oauth_token_secret;
+
+        public string OauthTokenSecret
+        {
+            get { return oauth_token_secret; }
+            set { oauth_token_secret = value; }
+        }
+
+        public RequestToken(string requestToken)
+        {
+            this.requestToken = requestToken;
+            SplitRequestToken(this.requestToken);
+        }
+        /// <summary>
+        /// Splits the request token into oauth_token and oauth_token_secret
+        /// </summary>
+        /// <param name="request"></param>
+        private void SplitRequestToken(string request)
+        {
+            if (request != null)
+            {
+                String[] keyValPairs = request.Split('&');
+
+                for (int i = 0; i < keyValPairs.Length; i++)
+                {
+                    String[] splits = keyValPairs[i].Split('=');
+                    switch (splits[0])
+                    {
+                        case "oauth_token":
+                            OauthToken = splits[1];
+                            break;
+                        case "oauth_token_secret":
+                            OauthTokenSecret = splits[1];
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region Auth Token Class
+    class AuthToken
+    {
+        string authToken;
+        
+        string oauth_token;
+        public string OauthToken
+        {
+            get { return oauth_token; }
+            set { oauth_token = value; }
+        }
+        string oauth_verifier;
+
+        public string OauthVerifier
+        {
+            get { return oauth_verifier; }
+            set { oauth_verifier = value; }
+        }
+
+        public AuthToken(string authToken)
+        {
+            this.authToken = authToken;
+            SplitAuthToken(this.authToken);
+        }
+        /// <summary>
+        /// Splits the authentication token into oauth_token and oauth_verifier
+        /// </summary>
+        /// <param name="auth"></param>
+        private void SplitAuthToken(string auth)
+        {
+            if (auth != null)
+            {
+                String[] keyValPairs = auth.Split('&');
+
+                for (int i = 0; i < keyValPairs.Length; i++)
+                {
+                    String[] splits = keyValPairs[i].Split('=');
+                    switch (splits[0])
+                    {
+                        case "oauth_token":
+                            OauthToken = splits[1];
+                            break;
+                        case "oauth_verifier":
+                            OauthVerifier = splits[1];
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+}
---
+++ Splitwise/Service Classes/SplitwiseHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitwise_New.Service_Classes
+{
+    public static class SplitwiseHelper
+    {
+        #region Uncategorized API
+        public void GetCurrencies
+        {
+        }
+
+        public void GetCategories
+        {
+        }
+
+        public void ParseSentence
+        {
+        }
+        #endregion
+
+        #region User API
+        public void GetCurrentUser
+        {
+        }
+
+        public void GetUser
+        {
+        }
+
+        public void UpdateUser
+        {
+        }
+        #endregion
+
+        #region Groups API
+        public void GetGroups
+        {
+        }
+
+        public void GetGroup
+        {
+        }
+
+        public void CreateGroup
+        {
+        }
+    
+        public void AddUserToGroup
+        {
+        }
+
+        public void RemoveUserFromGroup
+        {
+        }
+        #endregion
+
+        #region Expenses API
+        public void GetExpenses
+        {
+        }
+    
+        public void GetExpense
+        {
+        }
+
+        public void CreateExpense
+        {
+        }
+
+        public void UpdateExpense
+        {
+        }
+
+        public void DeleteExpense
+        {
+        }
+        #endregion
+
+        #region Friendships API
+        public void GetFriendships
+        {
+        }
+
+        public void GetFriendship
+        {
+        }
+
+        public void CreateFriendships
+        {
+        }
+
+        public void DeleteFriendship
+        {
+        }
+        #endregion
+    }
+}
